b0aa50d5675ac5ec53f965670e26d756511cbeb7
Lucas Lago <lucasbenderlago@gmail.com>
2018-09-20 16:55:45 -0300

    Change import order

--- src/components/Hello.test.tsx
+++ src/components/Hello.test.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
 import * as enzyme from 'enzyme';
+import * as React from 'react';
 import Hello from './Hello';
 
 it( 'renders the correct text when no enthusiasm level is given', () => {
