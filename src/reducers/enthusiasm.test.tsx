c2094d1b448a01e308374c295508cc3d18736e75
Lucas Lago <lucasbenderlago@gmail.com>
2018-09-23 22:29:19 -0300

    Remove unnecessary describes

--- src/reducers/enthusiasm.test.tsx
+++ src/reducers/enthusiasm.test.tsx
@@ -1,18 +1,14 @@
 import enthusiasm from './enthusiasm';
 
-describe( 'when given "INCREMENT_ENTHUSIASM" action', () => {
-  it( 'adds 1 to enthusiasmLevel', () => {
-    const nextState = enthusiasm( { enthusiasmLevel: 1, languageName: 'TypeScript' }, { type: 'INCREMENT_ENTHUSIASM' } );
-    expect( nextState ).toEqual( { enthusiasmLevel: 2, languageName: 'TypeScript' } );
-  } );
+it( 'adds 1 to enthusiasmLevel', () => {
+  const nextState = enthusiasm( { enthusiasmLevel: 1, languageName: 'TypeScript' }, { type: 'INCREMENT_ENTHUSIASM' } );
+  expect( nextState ).toEqual( { enthusiasmLevel: 2, languageName: 'TypeScript' } );
 } );
-describe( 'when given "DECREMENT_ENTHUSIASM" action', () => {
-  it( 'subtracts 1 of enthusiasmLevel', () => {
-    const nextState = enthusiasm( { enthusiasmLevel: 2, languageName: 'TypeScript' }, { type: 'DECREMENT_ENTHUSIASM' } );
-    expect( nextState ).toEqual( { enthusiasmLevel: 1, languageName: 'TypeScript' } );
-  } );
-  it( 'does not permit decrementing enthusiasmLevel below 1', () => {
-    const nextState = enthusiasm( { enthusiasmLevel: 1, languageName: 'TypeScript' }, { type: 'DECREMENT_ENTHUSIASM' } );
-    expect( nextState ).toEqual( { enthusiasmLevel: 1, languageName: 'TypeScript' } );
-  } );
+it( 'subtracts 1 of enthusiasmLevel', () => {
+  const nextState = enthusiasm( { enthusiasmLevel: 2, languageName: 'TypeScript' }, { type: 'DECREMENT_ENTHUSIASM' } );
+  expect( nextState ).toEqual( { enthusiasmLevel: 1, languageName: 'TypeScript' } );
+} );
+it( 'does not permit decrementing enthusiasmLevel below 1', () => {
+  const nextState = enthusiasm( { enthusiasmLevel: 1, languageName: 'TypeScript' }, { type: 'DECREMENT_ENTHUSIASM' } );
+  expect( nextState ).toEqual( { enthusiasmLevel: 1, languageName: 'TypeScript' } );
 } );
